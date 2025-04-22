93b032893a314c8284206d1beeb14bb906ef3a3c
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-04-22 14:41:44 +0200

    add snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,18 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+// todo - soluzione
+
+// const addNumbers = numbers.map((number) => {
+//   number += 1;
+//   return number;
+// });
+
+// console.log(addNumbers);
+
+// # FORMA COMPATTA
+
+const addNumbers = numbers.map((number) => (number += 1));
+
+console.log(addNumbers);
