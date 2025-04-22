c1d5179b3efda69596ca1167e936fe1e3489d269
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-04-22 15:48:34 +0200

    add snack 1 e 2

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,9 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+// todo - soluzione
+
+const namePeople = names.map((name) => name).join(`, `);
+console.log(namePeople);
