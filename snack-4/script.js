366626e34de3d01b5088332316a5f0ce48049209
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-04-22 14:58:47 +0200

    add snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -1,9 +1,23 @@
 const posts = [
-  { author: 'Marco', date: '12/3/2021', content: 'lorem ipsum...' },
-  { author: 'Luca', date: '30/6/2021', content: 'lorem ipsum...' },
-  { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
-  { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
-]
+  { author: "Marco", date: "12/3/2021", content: "lorem ipsum..." },
+  { author: "Luca", date: "30/6/2021", content: "lorem ipsum..." },
+  { author: "Fabrizio", date: "12/9/2020", content: "lorem ipsum..." },
+  { author: "Enrico", date: "09/12/2021", content: "lorem ipsum..." },
+];
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+// todo - soluzione
+
+// const authorName = posts.map((name) => {
+
+//   return name.author;
+// });
+
+// console.log(authorName);
+
+// # forma compatta
+
+const authorName = posts.map((name) => name.author);
+console.log(authorName);
